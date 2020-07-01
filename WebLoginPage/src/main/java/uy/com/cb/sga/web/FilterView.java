91f42df7bb8f8dce18f0e0bf285901382e6122b6
Carlos Barbieri <carlosdelviento23@gmail.com>
2020-07-01 18:31:39 -0300

    se agrega buscador de personas y se corrigen listado de personas

---
+++ WebLoginPage/src/main/java/uy/com/cb/sga/web/FilterView.java
@@ -0,0 +1,92 @@
+package uy.com.cb.sga.web;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import uy.com.cb.sga.domain.Persona;
+import uy.com.cb.sga.servicio.PersonaService;
+
+@Named("dtFilterView")
+@ViewScoped
+public class FilterView implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Persona> personas;
+
+	private List<Persona> filteredPersons1;
+
+	@Inject
+	private PersonaService personaService;
+
+	@PostConstruct
+	public void inicializar() {
+		// Iniciamos las variables
+		personaService.listarPersonas();
+	}
+
+	public boolean filterByNombre(Object value, Object filter, Locale locale) {
+		String filterText = (filter == null) ? null : filter.toString().trim();
+		if (filterText == null || filterText.equals("")) {
+			return true;
+		}
+
+		if (value == null) {
+			return false;
+		}
+
+		return ((Comparable) value).compareTo(getInteger(filterText)) > 0;
+	}
+
+	/*public boolean globalFilterFunction(Object value, Object filter, Locale locale) {
+		String filterText = (filter == null) ? null : filter.toString().trim().toLowerCase();
+		if (filterText == null || filterText.equals("")) {
+			return true;
+		}
+		int filterInt = getInteger(filterText);
+
+		Persona persona = (Persona) value;
+		/*return persona.getIdPersona().toLowerCase().contains(filterInt)
+				|| persona.getNombre().toLowerCase().contains(filterText)
+				|| persona.getApellido().toLowerCase().contains(filterText)
+				|| persona.getEmail()
+				|| persona.getFecha();
+	}*/
+
+	private int getInteger(String string) {
+		try {
+			return Integer.valueOf(string);
+		}
+		catch (NumberFormatException ex) {
+			return 0;
+		}
+	}
+
+	public List<Persona> getPersonas() {
+		return personas;
+	}
+
+	public void setPersonas(List<Persona> personas) {
+		this.personas = personas;
+	}
+
+	public List<Persona> getFilteredPersons1() {
+		return filteredPersons1;
+	}
+
+	public void setFilteredPersons1(List<Persona> filteredPersons1) {
+		this.filteredPersons1 = filteredPersons1;
+	}
+
+	public void setPersonaService(PersonaService personaService) {
+		this.personaService = personaService;
+	}
+
+
+}
--- test.sql
+++ test.sql
@@ -18,33 +18,81 @@ USE `test`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `persona`
+--
+
+DROP TABLE IF EXISTS `persona`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `persona` (
+    `id_persona` INT(4) NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(45) NOT NULL,
+    `apellido` VARCHAR(45) NOT NULL,
+    `email` VARCHAR(45) NOT NULL,
+    `telefono` VARCHAR(45) NOT NULL,
+    `fecha` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id_persona`),
+    UNIQUE KEY `email_UNIQUE` (`email`)
+)  ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8MB4 COLLATE = UTF8MB4_0900_AI_CI;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `persona`
+--
+
+LOCK TABLES `persona` WRITE;
+/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
+INSERT INTO `test`.`persona` (`id_persona`,`nombre`, `apellido`, `email`, `telefono`) VALUES 
+('1','Juan','Perez','jperez@mail.com','5513445567'),
+('2','Karla','Gomez','kgomez@mail.com','7713445678'),
+('3','Maria','Gutierrez','mariagutierrez@mail.com','88991199'),
+('4','Diego','Gutierrez','dgutierrez@mail.com','99999999'),
+('5','Pablo','Gutierrez','pgutierrez@mail.com','88991197'),
+('6','Julio','Gutierrez','jgutierrez@mail.com','88991197'),
+('7','Hector','Gutierrez','hgutierrez@mail.com','88991159'),
+('8','Rosa','Gutierrez','rgutierrez@mail.com','889911399'),
+('9','Patricia','Gutierrez','patgutierrez@mail.com','88921199'),
+('10','Mirta','Gutierrez','mirtagutierrez@mail.com','88991199'),
+('11','Gonzalo','Gutierrez','ggutierrez@mail.com','88991199'),
+('12','Peter','Gutierrez','putierrez@mail.com','88991199'),
+('13','Federico','Gutierrez','fgutierrez@mail.com','88991199'),
+('14','Lurdes','Gutierrez','lurdesgutierrez@mail.com','88991199'),
+('15','Jenny','Gutierrez','jengutierrez@mail.com','88991119'),
+('16','Jessica','Gutierrez','jesgutierrez@mail.com','18991199'),
+('17','Luisa','Gutierrez','luisagutierrez@mail.com','88091199'),
+('18','Felipe','Gutierrez','felipegutierrez@mail.com','88991169'),
+('19','Pepe','Gutierrez','pepegutierrez@mail.com','18991199'),
+('20','Pepa','Gutierrez','pepagutierrez@mail.com','28991199');
+/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `usuario`
 --
 
-DROP TABLE IF EXISTS `usuarios`;
+DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuarios` (
-  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
-  /*`id_persona` int(11) DEFAULT NULL,*/
-  `username` varchar(45) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `fecha` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_usuario`),
-  UNIQUE KEY `username` (`username`)
-  /*KEY `fk_usuario_persona_idx` (`id_persona`),*/
-  /*CONSTRAINT `fk_usuario_persona` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`) ON DELETE SET NULL*/
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `usuario` (
+    `id_usuario` INT(4) NOT NULL AUTO_INCREMENT,
+    `id_persona` INT(4),
+    `username` VARCHAR(45) NOT NULL,
+    `password` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id_usuario`),
+    KEY `fk_usuario_persona_idx` (`id_persona`),
+    CONSTRAINT `fk_usuario_persona` FOREIGN KEY (`id_persona`)
+        REFERENCES `persona` (`id_persona`)
+        ON DELETE SET NULL
+)  ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8MB4 COLLATE = UTF8MB4_0900_AI_CI;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `usuario`
 --
 
-LOCK TABLES `usuarios` WRITE;
+LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'admin','admin123');
-INSERT INTO `usuarios` VALUES (2,'usuario','usuario123');
+INSERT INTO `test`.`usuario` VALUES ('1','1','jperez','123'),('2','2','kgomez','123'),('3','3','admin','admin123');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,32 +106,3 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2019-08-19 22:13:41
-
---
--- Table structure for table `persona`
---
-
-DROP TABLE IF EXISTS `personas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `personas` (
-  `id_persona` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) NOT NULL,
-  `apellido` varchar(45) NOT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `telefono` varchar(45) DEFAULT NULL,
-  `fecha` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  PRIMARY KEY (`id_persona`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `persona`
---
-
-LOCK TABLES `personas` WRITE;
-/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
-INSERT INTO `personas` VALUES (1,'Juan','Perez','jperez@mail.com','5513445567'),(2,'Karla','Gomez','kgomez@mail.com','7713445678'),(3,'Maria','Gutierrez','mgutierrez@mail.com','88991199'),(4,'Diego','Gutierrez','dgutierrez@mail.com','99999999'),(5,'Pablo','Gutierrez','pgutierrez@mail.com','88991197'),(6,'Julio','Gutierrez','jgutierrez@mail.com','88991197'),(7,'Hector','Gutierrez','hgutierrez@mail.com','88991159'),(8,'Rosa','Gutierrez','rgutierrez@mail.com','889911399'),(9,'Patricia','Gutierrez','pgutierrez@mail.com','88921199'),(10,'Mirta','Gutierrez','mgutierrez@mail.com','88991199'),(11,'Gonzalo','Gutierrez','mgutierrez@mail.com','88991199'),(12,'Peter','Gutierrez','mgutierrez@mail.com','88991199'),(13,'Federico','Gutierrez','mgutierrez@mail.com','88991199'),(14,'Lurdes','Gutierrez','lgutierrez@mail.com','88991199'),(15,'Jenny','Gutierrez','jgutierrez@mail.com','88991119'),(16,'Jessica','Gutierrez','jgutierrez@mail.com','18991199'),(17,'Luisa','Gutierrez','lgutierrez@mail.com','88091199'),(18,'Felipe','Gutierrez','fgutierrez@mail.com','88991169'),(19,'Pepe','Gutierrez','pgutierrez@mail.com','18991199'),(20,'Pepa','Gutierrez','pepagutierrez@mail.com','28991199'),(21,'Pepo','Gutierrez','pepogutierrez@mail.com','988991199'),(22,'Tita','Gutierrez','tgutierrez@mail.com','28991199'),(23,'Tito','Gutierrez','titogutierrez@mail.com','8238991199'),(24,'Tete','Gutierrez','tetegutierrez@mail.com','558991199'),(25,'Mario','Gutierrez','mgutierrez@mail.com','889911923'),(26,'Marito','Gutierrez','maritogutierrez@mail.com','23488991199'),(27,'Marita','Gutierrez','maritagutierrez@mail.com','455488991199'),(28,'Fernanda','Gutierrez','fgutierrez@mail.com','213488991199'),(29,'Fernando','Gutierrez','fgutierrez@mail.com','5656454688991199'),(30,'Fernandito','Gutierrez','fernanditogutierrez@mail.com','4354588991199'),(31,'Tomas','Gutierrez','tomgutierrez@mail.com','65767788991199'),(32,'Tomasito','Gutierrez','tsitogutierrez@mail.com','6666688991199'),(33,'Tamara','Gutierrez','tamaragutierrez@mail.com','534534588991199'),(34,'Silvio','Gutierrez','silviogutierrez@mail.com','77788991199'),(35,'Silvia','Gutierrez','sgutierrez@mail.com','77788991199'),(36,'Silverio','Gutierrez','silveriogutierrez@mail.com','6464588991199'),(37,'Walter','Gutierrez','wgutierrez@mail.com','888888991199'),(38,'Washington','Gutierrez','wgutierrez@mail.com','8899119988885'),(39,'Rodrigo','Gutierrez','rgutierrez@mail.com','0000088991199'),(40,'Roberto','Gutierrez','rgutierrez@mail.com','6767688991199');
-/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
-UNLOCK TABLES;
